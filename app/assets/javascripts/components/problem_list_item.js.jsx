044a1bc155b80c7d83f3ff0de4dfb3837dd55978
Hugo Alves <goninja@gmail.com>
2016-09-15 10:53:03 +0100

    com mudancas para reveretr

--- app/assets/javascripts/components/problem_list_item.js.jsx
+++ app/assets/javascripts/components/problem_list_item.js.jsx
@@ -9,9 +9,12 @@ var ProblemListItem = React.createClass({
         <div className="mdl-card__title">
           <h1><a href={Routes.problem_path(this.props.problem)} >{this.props.problem.title}</a></h1>
             <a><i className="material-icons">share</i></a>
-          </div>
-          <div className="mdl-card__actions mdl-card--border">
+        </div>
+        <div className="mdl-card--content">
           <p>{this.props.problem.description}</p>
+
+        </div>
+        <div className="mdl-card__actions mdl-card--border">
           <footer>
             <p>By: {this.props.problem.name}</p>
           </footer>

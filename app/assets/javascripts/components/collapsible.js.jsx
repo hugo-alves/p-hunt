e9c6eb2669711d5e21a4f9fcc14751fcb5779302
tauan lemos <tauanmiguel@gmail.com>
2016-09-16 14:11:16 +0100

    problems

--- app/assets/javascripts/components/collapsible.js.jsx
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  console.log($('.collapsible'))
-  $('.collapsible').collapsible({
-    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-  });
-});
--- app/assets/javascripts/components/problem_list.js.jsx
+++ app/assets/javascripts/components/problem_list.js.jsx
@@ -1,11 +1,17 @@
 var ProblemList = React.createClass({
+  getInitialState: function() {
+    return {
+        $('.collapsible').on("click", function() {
+          $(this).toggleClass("active");
+    }
+  }
   render: function() {
     return (
       <div>
         <ul className="collapsible popout" data-collapsible="accordion">
           {this.props.problems.map(function(problem){
             return <ProblemListItem problem={problem} key={problem.id} />;
-          })}
+          })};
         </ul>
       </div>
     );

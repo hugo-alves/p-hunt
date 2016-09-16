f2ed440445b0fd899b4987c7d52bc723d7a29cf8
tauan lemos <tauanmiguel@gmail.com>
2016-09-16 16:08:38 +0100

    MVP is ready

--- app/assets/javascripts/components/problem_list_item.js.jsx
+++ app/assets/javascripts/components/problem_list_item.js.jsx
@@ -3,10 +3,10 @@ var ProblemListItem = React.createClass({
     return (
       <li className="collection-item">
         <span>
-          <a href={Routes.problem_path(this.props.problem)} >{this.props.problem.title}</a>
+          <a className="title"href={Routes.problem_path(this.props.problem)} >{this.props.problem.title}</a>
         </span>
         <Upvote problem={this.props.problem}  />
-        <p>By: {this.props.problem.name}</p>
+        <p className="author">By: {this.props.problem.name}</p>
       </li>
     );
   }
